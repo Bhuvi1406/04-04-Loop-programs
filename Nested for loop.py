33db90ab3c2cc404e15c67ff68555e54cd9c23de
Bhuvi1406 <118524851+Bhuvi1406@users.noreply.github.com>
2024-04-04 21:37:59 +0530

    Programs Uploaded

---
+++ Nested for loop.py
@@ -0,0 +1,4 @@
+for i in range(1,3):
+    print("Week: ",i)
+    for j in range(1,4):
+       print("    Day: ",j)
---
+++ While loop.py
@@ -0,0 +1,7 @@
+i=int(input())
+count=1
+while(0<i):
+    count=count*i
+    i=i-1
+print(count,end=" ")
+   
